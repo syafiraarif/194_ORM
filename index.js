fe0aa1ffd00042c1802ce4f57bbf4355ba300fb3
syairaarif <putrisyafira4444@gmail.com>
2025-10-28 14:48:00 +0700

    menambahkan  route DELETE /komik untuk menghapus data Komik dari database

--- index.js
+++ index.js
@@ -53,4 +53,18 @@ app.put('/komik/:id', async (req, res) => {
     }
 });
 
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try{
+        const komik = await db.Komik.findByPk(id);
+        if(!komik){
+            return res.status(404).send({message: "Komik not found"});
+        }
+        await komik.destroy();
+        res.send({message: "Komik berhasil dihapus"}, komik);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
 
